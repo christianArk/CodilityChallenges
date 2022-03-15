acd59a8ebc9abf7ae1bd146c04c1dc2c04f4fdf0
Onyeneke Christian <onyenekechristian@gmail.com>
2022-03-15 07:16:35 +0100

    Update CountDiv.cs

--- CountDiv.cs
+++ CountDiv.cs
@@ -1,1 +1,12 @@
+using System;
+// you can also use other imports, for example:
+// using System.Collections.Generic;
 
+// you can write to stdout for debugging purposes, e.g.
+// Console.WriteLine("this is a debug message");
+
+class Solution {
+    public int solution(int A, int B, int K) {
+        return (B/K) - (A/K) + ((A % K == 0)? 1 : 0);
+    }
+}
